564b3807e98dcc5183de095ac933bb04366f4a25
Arkendra <arkendra.bhattacharya@gmail.com>
2026-03-15 21:39:34 +0000

    update

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,25 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value:
+              "default-src 'self' https:; " +
+              "script-src 'self' 'unsafe-eval' 'unsafe-inline' https:; " +
+              "style-src 'self' 'unsafe-inline' https:; " +
+              "img-src 'self' data: https:; " +
+              "connect-src 'self' https:; " +
+              "font-src 'self' data: https:; " +
+              "frame-src https:; ",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
